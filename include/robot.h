0645de87335b6db4ea795371b4f4793f58d81a7d
Daniel <49901842+DanielIngham@users.noreply.github.com>
2025-04-12 19:26:51 +0200

    Create Robots class to serve as the data structure housing all robot robot related functionality. This was done to improve readability of the data handler.

--- include/robot.h
+++ include/robot.h
@@ -1,7 +1,10 @@
 #ifndef INCLUDE_INCLUDE_ROBOT_H_
 #define INCLUDE_INCLUDE_ROBOT_H_
+#include <vector>
+
 
 class Robot {
+private:
 public:
 	Robot();
 	Robot(Robot &&) = default;
@@ -10,14 +13,78 @@ public:
 	Robot &operator=(const Robot &) = default;
 	~Robot();
 
-private:
-	
+	int id;			///< Numerical identifier for the robot. 
+	int barcode;		///< Barcode associated with the robot. This is what the other robots will read during there operation to identify each other.
+
+	/**
+	 * @brief Data attributes for a single groundtruth reading extracted from Robotx_Groundtruth.dat.
+	 * @note According the UTIAS website, the groundtruth readings are accurate to the order of 1mm (1E^-3 m). Additionally, the NTP deamon used to synchronise the clocks between robots has an average timing error of 1ms (1E-3 s).
+	 */
+	struct State {
+		/* The following attributes are extracted directly from the robots groundtruth datafile. */
+		double time;			///< Time stamp of the ground truth reading [s].
+
+		double x;			///< Robot Groundtruth x-coordinate [m].
+		double y;			///< Robot Groundtruth y-coordinate [m].
+		double orientation;		///< Robot Groundtruth orientation [rad].
+		 
+		State();
+		/** 
+		 * @brief Constructor for convenient population of DataExtractor::robots_.raw.ground_truth .
+		 */
+		State(double time_, double x_, double y_, double orientation_): time(time_), x(x_), y(y_), orientation(orientation_) {}
+	};
+
+	/**
+	 * @brief Data attributes of a single odometry reading extracted from Robotx_Odometry.dat.
+	 */
+	struct Odometry {
+		double time;			///< Time stamp of the odometry readings [s].
+		double forward_velocity;	///< The robots forward velocity [m/s].
+		double angular_velocity;	///< The robots angular velocity [rad/s].
+
+		Odometry();
+		/** 
+		 * @brief Constructor for convenient population of DataExtractor::robots_.raw.odometry .
+		 */
+		Odometry(double time_, double forward_velocity_, double angular_velocity_): time(time_), forward_velocity(forward_velocity_), angular_velocity(angular_velocity_) {} ;
+	};
+
+	/**
+	 * @brief Data attributes of the relative distance measurement taken at a single timestep in Robotx_Measurements.
+	 * @note This structure combines multiple measurements with the same time stamp. There it is assumed that the vectors: subjects, ranges, and bearings are in the same order.
+	 */
+	struct Measurement {
+		double time;			///< Time stamp of the measurement [s].
+		std::vector<int> subjects;	///< The Barcode of the other robots being measured.
+		std::vector<double> ranges;	///< The measured ranges to the subjects [m]
+		std::vector<double> bearings;	///< The bearings from the subjects [rad]
+
+		Measurement(double time_,  std::vector<int> subjects_, std::vector<double> ranges_, std::vector<double> bearings_): time(time_), subjects(subjects_), ranges(ranges_), bearings(bearings_) {};
+		/** 
+		 * @brief Constructor for convenient population of DataExtractor::robots_.raw.measurements .
+		 */
+		Measurement(double time_,  int subject_, double range_, double bearing_): time(time_){
+			subjects.push_back(subject_);
+			ranges.push_back(range_);
+			bearings.push_back(bearing_);
+		};
+	};
+
+	/**
+	 * @brief struct containing vectors related to the robot data files.
+	 */
+	struct {
+		std::vector<State> states;		///< All groundtruth values extracted for the given robot.
+		std::vector<Odometry> odometry;		///< All odometry inputs extracted for the given robot.
+		std::vector<Measurement> measurements;	///< All measurements taken by the given robot.
+	} raw,		///< The raw data extracted from the dataset's .dat files. 
+	synced,		///< The odometry and measurement values with synced timesteps.
+	groundtruth,	///< The groundtruth
+	error;		///< The difference between the ground truth and the synced data
+
+	void calculateOdometryError();
+	void calculateMeasurementError();
 };
-
-Robot::Robot() {
-}
-
-Robot::~Robot() {
-}
 
 #endif  // INCLUDE_INCLUDE_ROBOT_H_
---
+++ src/robot.cpp
@@ -0,0 +1,43 @@
+#include <../include/robot.h>
+#include <stdexcept>
+#include <string>
+
+Robot::Robot() {
+}
+
+Robot::~Robot() {
+}
+
+/**
+ * @brief Calculates the absolute error between the groundtruth odometry values and the values measured.
+ */
+void Robot::calculateOdometryError() {
+
+	/* Check if the groundtruth has been set. */
+	if (this->groundtruth.states.size() == 0) {
+		throw std::runtime_error("Groundtruth state values for robot" + std::to_string(this->id) + " have not been set."); 
+	}
+
+	/* Check if the synced data has been set. */
+	if (this->synced.states.size() == 0) {
+		throw std::runtime_error("Synced values for robot" + std::to_string(this->id) + " have not been set."); 
+	}
+
+	/* If the odometry error vector is not empty, empty it before calculation. */
+	if (this->error.odometry.size() > 0) {
+		this->error.odometry.clear();
+	}
+
+	/* Calculate groundtruth error. */
+	for (std::size_t k = 0; k < this->groundtruth.states.size() - 1; k++) {
+		/* Ignore stationary odometry values before the system starts and after it ends. These readings cause a disproportionate amount of zero error readings. */
+		if (this->synced.odometry[k].angular_velocity != 0 && this->synced.odometry[k].forward_velocity != 0) {
+			this->error.odometry.push_back( Odometry(
+				this->groundtruth.odometry[k].time,
+				this->groundtruth.odometry[k].forward_velocity - this->synced.odometry[k].forward_velocity,
+				this->groundtruth.odometry[k].angular_velocity - this->synced.odometry[k].angular_velocity
+			));
+		}
+	}
+
+}
