22e86db6de8f961b12e2e08d5ff34dba2735d522
Daniel <49901842+DanielIngham@users.noreply.github.com>
2025-04-14 21:53:15 +0200

    Implement measurement error calculation function.

--- src/robot.cpp
+++ src/robot.cpp
@@ -1,6 +1,4 @@
 #include <../include/robot.h>
-#include <stdexcept>
-#include <string>
 #include <iostream>
 
 Robot::Robot() {
@@ -40,7 +38,45 @@ void Robot::calculateOdometryError() {
 			));
 		}
 	}
-
 }
 
 
+void Robot::calculateMeasurementError() {
+
+	/* Check if the groundtruth has been set. */
+	if (this->groundtruth.measurements.size() == 0) {
+		throw std::runtime_error("Groundtruth state values for robot " + std::to_string(this->id) + " have not been set."); 
+	}
+
+	/* Check if the synced data has been set. */
+	if (this->synced.measurements.size() == 0) {
+		throw std::runtime_error("Synced values for robot " + std::to_string(this->id) + " have not been set."); 
+	} 
+
+	/* If the measurement error vector is not empty, empty it before calculation. */
+	if (this->error.measurements.size() > 0) {
+		this->error.measurements.clear();
+	}
+
+	/* Calculate groundtruth error. */
+	auto iterator = this->error.measurements.begin();
+	for (std::size_t k = 0; k < this->groundtruth.measurements.size() - 1; k++) {
+		/* Loop through the subjects */
+		for (std::size_t s = 0; s < this->groundtruth.measurements[k].subjects.size(); s++) {
+			if (s == 0) {
+				this->error.measurements.push_back( Measurement(
+					this->groundtruth.measurements[k].time,
+					this->groundtruth.measurements[k].subjects[s],
+					this->groundtruth.measurements[k].ranges[s] - this->synced.measurements[k].ranges[s],
+					this->groundtruth.measurements[k].bearings[s] - this->synced.measurements[k].bearings[s]
+				));
+				iterator = this->error.measurements.end() - 1;
+				continue;
+			}
+
+			iterator->subjects.push_back(this->groundtruth.measurements[k].subjects[s]);
+			iterator->ranges.push_back(this->groundtruth.measurements[k].ranges[s] - this->synced.measurements[k].ranges[s]);
+			iterator->bearings.push_back(this->groundtruth.measurements[k].bearings[s] - this->synced.measurements[k].bearings[s]);
+		}
+	}
+}
